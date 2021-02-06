d4d85465bc3bd74caaaa6ea2d2f067f0c5660a17
noelsj007 <78490433+noelsj007@users.noreply.github.com>
2021-02-06 20:59:14 +0530

    Update views.py

--- cart/views.py
+++ cart/views.py
@@ -4,11 +4,12 @@ from cart.models import Product
 from homepage.models import Item
 
 def AddToCart(request):
-    cartItem = Item.objects.get(pk = Product_id)
-    print("product id", Product_id)
-    cartItem = Item()
-    cartItem = Item.objects.filter(id=product_id)
+    #cartItem = Item.objects.get(pk = Product_id)
+    #print("product id", Product_id)
+    #cartItem = Item()
+    #cartItem = Item.objects.filter(id=product_id)
     
     
     return render(request, 'dropdown.html')
+    
     
