bede460074cf8dac450281462dc3844ba81ee7f4
noelsj007 <78490433+noelsj007@users.noreply.github.com>
2021-02-06 14:31:59 +0530

    Update models.py

--- cart/models.py
+++ cart/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Product(models.Model):
     product_name = models.CharField(max_length=100)
-    """product_image = models.ImageField(upload_to='cartImg')"""
+    product_image = models.ImageField(upload_to='cartImg')
     product_price = models.IntegerField()
     product_publisher = models.CharField(max_length=100, null=True)
     product_order_date = models.DateField(auto_now=True)
